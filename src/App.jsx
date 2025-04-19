0b36fe3c198cf214ba0c4f0157af91a97483f281
sardor-755 <sardormaxmudjonov0@gmail.com>
2025-04-19 11:10:22 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+       <h1>Sardor Maxmudjonov</h1>
+     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla aliquid ipsa modi ea tempora?</p>
+     <ul>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>Tailwind css</li>
+      <li>Javascript</li>
+      <li>React</li>
+     </ul>
+    </>
+  )
+}
+
+export default App
